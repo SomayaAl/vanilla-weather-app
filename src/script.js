26e2d139366eac60615fb79ea1b7d6746d7ac4e4
SomayaAl <102401317+SomayaAl@users.noreply.github.com>
2022-04-15 17:15:19 -0400

    Added displayTemperature and formatDate functions

--- src/script.js
+++ src/script.js
@@ -1,6 +1,63 @@
-function displayTemperature(response) {}
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = String(date.getHours()).padStart(2, "0");
+  let minutes = String(date.getMinutes()).padStart(2, "0");
 
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
+function formatSunrise(timestamp) {
+  let date = new Date(timestamp);
+  let hours = String(date.getHours()).padStart(2, "0");
+  let minutes = String(date.getMinutes()).padStart(2, "0");
+
+  return `${hours}:${minutes}`;
+}
+
+function formatSunset(timestamp) {
+  let date = new Date(timestamp);
+  let hours = String(date.getHours()).padStart(2, "0");
+  let minutes = String(date.getMinutes()).padStart(2, "0");
+
+  return `${hours}:${minutes}`;
+}
+
+function displayTemperature(response) {
+  let cityElement = document.querySelector("#city");
+  let temperatureElement = document.querySelector("#temp");
+  let dateElement = document.querySelector("#date-time");
+  let descriptionElement = document.querySelector("#weather-description");
+  let highElement = document.querySelector("#high-temp");
+  let lowElement = document.querySelector("#low-temp");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let sunriseElement = document.querySelector("#sunrise");
+  let sunsetElement = document.querySelector("#sunset");
+
+  cityElement.innerHTML = response.data.name;
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  highElement.innerHTML = Math.round(response.data.main.temp_max);
+  lowElement.innerHTML = Math.round(response.data.main.temp_min);
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  sunriseElement.innerHTML = formatSunrise(response.data.sys.sunrise * 1000);
+  sunsetElement.innerHTML = formatSunset(response.data.sys.sunset * 1000);
+}
+
+let city = "Laurel";
 let apiKey = "7d81cb66d2a78969cfec2f704335508f";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=imperial`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
 
 axios.get(apiUrl).then(displayTemperature);
